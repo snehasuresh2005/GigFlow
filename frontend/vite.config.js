59a1e54c51027a91a2455c2a67b92c3c20845593
S Sneha <snehasuresh358@gmail.com>
2026-01-13 20:14:19 +0530

    Remove base path for Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GigFlow/',   // 👈 ADD THIS LINE (very important)
-
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
